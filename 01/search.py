54e96676bc580b7e2eee11ff412b6786138d3cab
Irina Vidal Migallón <irina.vidal+github@gmail.com>
2020-11-19 23:39:49 +0100

    search patterns
    type

--- 01/search.py
+++ 01/search.py
@@ -1,21 +1,37 @@
+import re
 from fizzbuzz import fizzbuzz
 
-# NOTE: feel free to redesign the grammar for your pattern.
 """
 If the pattern is found, return the position and the remainder of the list to be examined.
 """
-def search_in_sequence(element: str, sequence: list):
-    # CHANGE: this must be a re comparison,
+
+
+def match_element(pattern: str, element: str) -> bool:
+    # CHANGE: this must be a regex comparison,
     # consider the 4 different possibilities --> different methods?
     # find string in list: sequence.index(element)
     # find 1ofN words: try the above twice
     # find any word: re find word
     # find digit: re find digit
-    pos = 3
-    return pos
+
+    if pattern == "*":
+        return True
+    if pattern == "#":
+        re_pattern = re.compile("[0-9]+")
+        match = re.fullmatch(re_pattern, element) is not None
+        return match
+    if "|" in pattern:
+        word1, word2 = pattern.split("|")
+        match = (word1 == element) or (word2 == element)
+        return match
+
+    # Anything else, it better match exactly what the pattern is.
+    # We're accepting even things other than just words.
+    match = pattern == element
+    return match
 
 
-def search(pattern, *, max_value=100_000):
+def search(pattern, *, max_value=100_000) -> int:
     ''' search for a pattern within the fizzbuzz sequence up to max_value
 
     Use the following (sample) grammar:
@@ -24,29 +40,31 @@ def search(pattern, *, max_value=100_000):
     - *: match any word
     - #: match only a number
 
-    Assume match needs to be exactly the pattern, as in no unrecognized elements inbetween those of the pattern
+    Assume match needs to be exactly the pattern, as in fullmatch
+    Returns the position (0-indexed) where the matched sequence starts
     '''
 
     fizzbuzz_string = fizzbuzz(max_value)
-    # first one is free in the sequence
 
     sequence = fizzbuzz_string.split(", ")
-    pos = search_in_sequence(pattern.pop(0), sequence)
-    if pos < 0:
+    matched_positions = [i for i, element in enumerate(sequence) if match_element(pattern[0], element)]
+
+    if len(matched_positions) < 1:
         return None
 
     # the rest must follow exactly. As soon as a pair mis-matches, we're out.
     # If there was a perfect match, we keep the position where it started
-    for p, s in zip(pattern, sequence):
-        new_pos = search_in_sequence(pattern.pop(0), [sequence[pos]])
-        if new_pos < 0:
-            return None
-    return pos
+    for pos in matched_positions:
+        subsequence = sequence[pos:]
+        matches = [match_element(p, s) for p, s in zip(pattern, subsequence) ]
+        if all(matches):
+            return pos
 
+    return None
 
 
 # look for four sequential plays that are
 #   fizz, then fizz OR buzz, then anything, then buzz
 pattern = ['fizz', 'fizz|buzz', '*', 'fizzbuzz']
 
-search(pattern, max_value=10)
+search(pattern, max_value=15)
--- 01/test_search.py
+++ 01/test_search.py
@@ -1,14 +1,34 @@
-from search import search, search_in_sequence
+from search import search, match_element
+
+
+def test_match_element():
+    pattern = "fizz|buzz"
+    element = "buzz"
+    assert match_element(pattern, element)
+
+
+def test_match_element_any():
+    pattern = "*"
+    element = "buzz"
+    assert match_element(pattern, element)
 
 
 def test_search_word():
     pattern = ["fizz"]
     # For 10:
     # 1, 2, fizz, 4, buzz, fizz, 7.nm, 8, fizz, buzz
-    assert search(pattern, max_value = 10) == 3
+    assert search(pattern, max_value=10) == 2
+
 
 def test_search():
     pattern = ["fizz", "*", "buzz"]
     # For 10:
-    # 1, 2, fizz, 4, buzz, fizz, 7.nm, 8, fizz, buzz
-    assert search(pattern, max_value = 10) == 3
+    # 1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz
+    assert search(pattern, max_value=10) == 2
+
+
+def test_search_not_first():
+    pattern = ["fizz", "#", "#"]
+    # For 10:
+    # 1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz
+    assert search(pattern, max_value=10) == 5
