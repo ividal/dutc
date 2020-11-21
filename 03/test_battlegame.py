6c97fbcfeb3928ed24754d245c7d2fbae4a35fb1
Irina Vidal Migallón <irina.vidal+github@gmail.com>
2020-11-21 18:13:58 +0100

    tests for new rep of status & ships

--- 03/test_battlegame.py
+++ 03/test_battlegame.py
@@ -3,16 +3,16 @@ from battlegame import Battlegame
 import numpy as np
 
 test_ship_board = np.array([
-    [0, 1, 1, 1],
-    [0, 0, 1, 1],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]], dtype=bool)
-
-test_state_board = np.array([
     [0, -1, -1, -1],
     [0, 0, 1, -1],
     [0, 0, 0, 0],
     [0, 0, 0, 0]], dtype=bool)
+
+test_state_board = np.array([
+    [0, 1, 1, 1],
+    [0, 0, 0, 1],
+    [0, 0, 0, 0],
+    [0, 0, -1, 0]], dtype=bool)
 
 
 def test_fill_left():
@@ -30,26 +30,20 @@ def test_fill_left():
 
 
 def test_fire():
-    ref = np.array([
-        [1, 1, 1, 1],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]], dtype=bool)
-
     g = Battlegame(4)
     g.ships = (test_ship_board, False * test_ship_board)
     g.states = (test_state_board, 8 * test_state_board)
 
-    assert True == g.fire(0, (1, 2))
+    assert g.fire(0, (1, 2))
     assert (g.ships[0] == np.array([
-        [0, 1, 1, 1],
-        [0, 0, 0, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]], dtype=bool)).all()
-
-    assert (g.states[0] == np.array([
         [0, -1, -1, -1],
         [0, 0, -1, -1],
         [0, 0, 0, 0],
         [0, 0, 0, 0]], dtype=bool)).all()
+
+    assert (g.states[0] == np.array([
+        [0, 1, 1, 1],
+        [0, 0, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, -1, 0]], dtype=bool)).all()
     assert g.check_lost(0)
